4d2f46a361e4269cacf58a41823cbbb8589773c3
Jeetu Bangari <jbangari16@gmail.com>
2022-12-15 20:13:26 +0530

    Initial commit

---
+++ src/pages/new/New.jsx
@@ -0,0 +1,58 @@
+import "./new.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import { DriveFolderUploadOutlined } from "@mui/icons-material";
+import { useState } from "react";
+
+const New = ({ inputs, title }) => {
+  const [file, setFile] = useState("");
+  console.log(file);
+
+  return (
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+          <h1>{title}</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img
+              src={
+                file
+                  ? URL.createObjectURL(file)
+                  : "https://t4.ftcdn.net/jpg/00/89/55/15/240_F_89551596_LdHAZRwz3i4EM4J0NHNHy2hEUYDfXc0j.jpg"
+              }
+              alt=""
+            />
+          </div>
+          <div className="right">
+            <form>
+              <div className="formInput">
+                <label htmlFor="file">
+                  Image : <DriveFolderUploadOutlined className="icon" />
+                </label>
+                <input
+                  type="file"
+                  id="file"
+                  style={{ display: "none" }}
+                  onChange={(e) => setFile(e.target.files[0])}
+                />
+              </div>
+              {inputs.map((input) => (
+                <div className="formInput" key={input.id}>
+                  <label>{input.label}</label>
+                  <input type={input.type} placeholder={input.placeholder} />
+                </div>
+              ))}
+              <button>Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default New;
---
+++ src/pages/single/Single.jsx
@@ -0,0 +1,57 @@
+import "./single.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import Chart from "../../components/chart/Chart";
+import List from "../../components/table/Table";
+
+const Single = () => {
+  return (
+    <div className="single">
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+        <div className="top">
+          <div className="left">
+            <div className="editButton">Edit</div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img
+                src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                alt=""
+                className="itemImg"
+              />
+              <div className="details">
+                <h1 className="itemTitle">John Doe</h1>
+                <div className="detailItem">
+                  <span className="itemKey">Email : </span>
+                  <span className="itemValue">john@doe.com</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Phone : </span>
+                  <span className="itemValue">+1 123-456-7890</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Address : </span>
+                  <span className="itemValue">Boston, Massachusetts</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Country : </span>
+                  <span className="itemValue">USA</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <Chart aspect={3 / 1} title="User Spending (Last 6 Months)" />
+          </div>
+        </div>
+        <div className="bottom">
+          <h1 className="title">Last Transactions</h1>
+          <List />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Single;
